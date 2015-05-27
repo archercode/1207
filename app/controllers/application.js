85f5d6b59efac4afc0f96c858bb86924546291dc
archercode <tonnyquintos@gmail.com>
2015-05-27 11:29:01 +0800

    revised product.js

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({	
-	  q: Ember.computed.mapBy('model', 'quantity'),
+	  q: Ember.computed.mapBy('model','quantity'),
   	count: Ember.computed.sum('q'),
     itemCounter: function() {
    // console.log(this);

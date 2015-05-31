dcc423671482dbff6e6534a2baa3291462e44e10
butchbarcelona <francesca.barcelona@gmail.com>
2015-05-31 19:50:01 +0800

    layout changed

--- app/models/product.js
+++ app/models/product.js
@@ -55,6 +55,66 @@ Product.reopenClass({
 			image: '/DFR0024.jpg',
 			//items: [1]
 		},
+		{
+			id: 4,
+			title: 'URM37 V4.0 Ultrasonic Sensor',
+			price: 400,
+			model: '10xx10',
+			description: '<section><section><section><div><div><div><p><strong>INTRODUCTION</strong></p></div><div><p> </p><p>This Motor shield  for Arduino uses L293 chip which allow to drive two 7-12V DC motors with maximum 1A current. This shield can be directly mount onto standard Arduino Duemilanove and Arduino Mega. </p><p> </p></div></div></div></section><section><div><div><div><p><strong>SPECIFICATION</strong></p></div><div><p> </p><ul><li>2 way 7-12V motor drive </li><li>Up to 1A current each way</li><li>PIN 4,5,6,7 are used to drive two DC motors</li><li>Support PWM speed control </li></ul><p> </p></div></div></div></section><section><div><div><div><p><strong>DOCUMENTS</strong></p></div><div><p> </p><ul><li><a href="http://www.dfrobot.com/wiki/index.php?title=Arduino_Motor_Shield_(L293)_(SKU:_DRI0001)">Manual</a></li></ul><div> </div></div></div></div></section><section><div><div><div><p><strong>SHIPPING LIST</strong></p></div><div><p> </p><ul><li>1A Motor Shield  For Arduino  x1</li></ul></div></div></div></section></section></section>',
+			type: 'boards',
+			image: '/DFR0009.jpg',
+			//items: [1]
+		},
+		{
+			id: 5,
+			title: 'DS18B20 Temperature Sensor',
+			price: 500,
+			model: '20xx10',
+			description: 'This module help you to detect the light density and reflect the analog voltage signal back to Arduino controller. You can set the threshold of voltage level to trig other unit on Arduino project',
+			type:'sensors',
+			image: '/DFR0017.jpg',
+			//items: [1]
+		},
+		{
+			id: 6,
+			title: 'Analog Ambient Light Sensor',
+			price: 900,
+			model: '30xx10',
+			description: 'This module help you to detect the light density and reflect the analog voltage signal back to Arduino controller. You can set the threshold of voltage level to trig other unit on Arduino project',
+			type: 'robotics',
+			image: '/DFR0024.jpg',
+			//items: [1]
+		},
+		{
+			id: 7,
+			title: 'URM37 V4.0 Ultrasonic Sensor',
+			price: 400,
+			model: '10xx10',
+			description: '<section><section><section><div><div><div><p><strong>INTRODUCTION</strong></p></div><div><p> </p><p>This Motor shield  for Arduino uses L293 chip which allow to drive two 7-12V DC motors with maximum 1A current. This shield can be directly mount onto standard Arduino Duemilanove and Arduino Mega. </p><p> </p></div></div></div></section><section><div><div><div><p><strong>SPECIFICATION</strong></p></div><div><p> </p><ul><li>2 way 7-12V motor drive </li><li>Up to 1A current each way</li><li>PIN 4,5,6,7 are used to drive two DC motors</li><li>Support PWM speed control </li></ul><p> </p></div></div></div></section><section><div><div><div><p><strong>DOCUMENTS</strong></p></div><div><p> </p><ul><li><a href="http://www.dfrobot.com/wiki/index.php?title=Arduino_Motor_Shield_(L293)_(SKU:_DRI0001)">Manual</a></li></ul><div> </div></div></div></div></section><section><div><div><div><p><strong>SHIPPING LIST</strong></p></div><div><p> </p><ul><li>1A Motor Shield  For Arduino  x1</li></ul></div></div></div></section></section></section>',
+			type: 'boards',
+			image: '/DFR0009.jpg',
+			//items: [1]
+		},
+		{
+			id: 8,
+			title: 'DS18B20 Temperature Sensor',
+			price: 500,
+			model: '20xx10',
+			description: 'This module help you to detect the light density and reflect the analog voltage signal back to Arduino controller. You can set the threshold of voltage level to trig other unit on Arduino project',
+			type:'sensors',
+			image: '/DFR0017.jpg',
+			//items: [1]
+		},
+		{
+			id: 9,
+			title: 'Analog Ambient Light Sensor',
+			price: 900,
+			model: '30xx10',
+			description: 'This module help you to detect the light density and reflect the analog voltage signal back to Arduino controller. You can set the threshold of voltage level to trig other unit on Arduino project',
+			type: 'robotics',
+			image: '/DFR0024.jpg',
+			//items: [1]
+		},
 	]
 });
 
--- app/views/products/product.js
+++ app/views/products/product.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  didInsertElement: function() {},
+  didInsertElement: function() {
+  	console.log("didInsertElement product specific");
+
+  	//todo: minimize navbar
+
+  	var navTexts = $(".nav_text");
+  	for(text in navTexts){
+  		text.hide();
+  	}
+
+  },
 });

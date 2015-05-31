57000841bfa4da2efcd56c5b119fb59e5c0962c9
archercode <tonnyquintos@gmail.com>
2015-05-31 22:33:51 +0800

    commiting for pull

--- app/routes/products/index.js
+++ app/routes/products/index.js
@@ -3,4 +3,9 @@ export default Ember.Route.extend({
 	model:function(){
 		return this.modelFor('products');
 	},
+	actions:{
+		loading: function(transition, originRoute) {
+        	return true;
+      },
+	},
 });
